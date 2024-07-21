4b4b052af8c3abb508930e8d503680a341fad13e
Nguyen Le Vinh <vikvenus199x@gmail.com>
2024-07-21 22:02:53 +0700

    [ASP.NET] Layout Views section

---
+++ LayoutViewExample/LayoutViewExample/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
