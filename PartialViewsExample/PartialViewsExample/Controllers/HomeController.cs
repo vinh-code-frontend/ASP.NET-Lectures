9dd406a0d9f8d18d45db8fa343bf6e2f29d38a7f
Nguyen Le Vinh <vikvenus199x@gmail.com>
2024-07-24 22:58:25 +0700

    [ASP.NET] Partial Views section

---
+++ PartialViewsExample/PartialViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewsExample.Models;
+
+namespace PartialViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewData["Data"] = new List<User>() {
+                new User ()
+                {
+                    Id = 1,
+                    Name ="Vinh",
+                    Job = "FE dev"
+                },
+                 new User ()
+                {
+                    Id = 2,
+                    Name ="Kim Anh",
+                    Job = "Broker"
+                }
+
+            };
+            return View();
+        }
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("friends")]
+        public IActionResult Friends()
+        {
+            List<User> users = new List<User>() {
+                new User()
+                {
+                    Id = 1,
+                    Name = "Tít",
+                    Job = "Ngủ"
+                },
+                 new User()
+                {
+                    Id = 2,
+                    Name = "Chụt",
+                    Job = "Ăn"
+                },
+                  new User()
+                {
+                    Id = 2,
+                    Name = "Ki",
+                    Job = "Chơi"
+                },
+                   new User()
+                {
+                    Id = 2,
+                    Name = "Lu",
+                    Job = "Lăn"
+                },
+                    new User()
+                {
+                    Id = 2,
+                    Name = "Ponny",
+                    Job = "Unknown"
+                },
+                     new User()
+                {
+                    Id = 2,
+                    Name = "Đấc",
+                    Job = "Quẹc quẹc"
+                },
+            };
+            return View(users);
+        }
+    }
+}
---
+++ PartialViewsExample/PartialViewsExample/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace PartialViewsExample.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Job { get; set; }
+    }
+}
