061bd274b53c589363fe2b8a542de62793b8a803
Nguyen Le Vinh <vikvenus199x@gmail.com>
2024-07-30 23:25:18 +0700

    [ASP.NET] Configuration section: Stock app

---
+++ StocksApp/StocksApp/Program.cs
@@ -0,0 +1,16 @@
+using StocksApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<MyService>();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+
+app.Run();
